26a4ddd6273f8f494a993fcdd33cc3f8f5576add
kobeomseok95 <kobumssh@naver.com>
2022-01-09 15:28:42 +0900

    initial commit

---
+++ src/main/java/com/danggn/challenge/DanggnmarketChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.danggn.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggnmarketChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanggnmarketChallengeApplication.class, args);
+    }
+
+}
