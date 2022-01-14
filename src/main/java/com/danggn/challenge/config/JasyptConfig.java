affb42d03b6d862221205c378da43ed44f399baa
kobeomseok95 <kobumssh@naver.com>
2022-01-14 14:00:02 +0900

    jasypt 환경 설정

---
+++ src/main/java/com/danggn/challenge/config/JasyptConfig.java
@@ -0,0 +1,28 @@
+package com.danggn.challenge.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimplePBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("jasypt.encryptor.password")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimplePBEConfig config = new SimplePBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
