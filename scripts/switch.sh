e13a0661ece62a6ddb89395f1bfd3a7f3bb1e3ef
kobeomseok95 <kobumssh@naver.com>
2022-01-27 14:14:00 +0900

    switch.sh > service ip를 변수로 받는 방식으로 변경

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -12,9 +12,12 @@ function switch_proxy() {
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
+
+    SERVICE_IP=$(hostname -I | awk '{print $1}')
+    echo "> IP 확인 : $SERVICE_IP"
     # nginx와 연결한 주소 생성
     # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
-    echo "set \$service_url http://${hostname -I | awk '{print $1}'}:${IDLE_PORT};" | sudo tee /home/ec2-user/app/nginx/service-url.inc
+    echo "set \$service_url http://${SERVICE_IP}:${IDLE_PORT};" | sudo tee /home/ec2-user/app/nginx/service-url.inc
 
     echo "> 엔진엑스 Reload"
     # nignx reload. restart와는 다르게 설정 값만 불러옴
