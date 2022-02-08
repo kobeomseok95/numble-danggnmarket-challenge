7441172d9d3513dab34477762dde662dd1f903c7
kobeomseok95 <kobumssh@naver.com>
2022-02-09 00:51:08 +0900

    wanna port 8082

--- src/main/java/com/danggn/challenge/DanggnmarketChallengeApplication.java
+++ src/main/java/com/danggn/challenge/DanggnmarketChallengeApplication.java
@@ -3,6 +3,7 @@ package com.danggn.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: 2022/02/09 kobeomseok95 docker-compose 기반으로 리팩토링해보기?
 @SpringBootApplication
 public class DanggnmarketChallengeApplication {
 
