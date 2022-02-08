ae0c465974e42900849001822d9cf99879e8a475
kobeomseok95 <kobumssh@naver.com>
2022-02-09 01:45:07 +0900

    AfterAllowTraffic 제거, clear.sh에 image 지우기 추가

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-echo "> nginx 이미지 제거"
-docker rmi -f $(docker images --filter "dangling=true" -q --no-trunc)
+function docker_image_clear() {
+  echo "> nginx 이미지 제거"
+  docker rmi -f $(docker images --filter "dangling=true" -q --no-trunc)
+}
--- scripts/health.sh
+++ scripts/health.sh
@@ -7,6 +7,7 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
+source ${ABSDIR}/clear.sh
 
 IDLE_PORT=$(find_idle_port)
 
@@ -23,6 +24,7 @@ do
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
+      docker_image_clear
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
